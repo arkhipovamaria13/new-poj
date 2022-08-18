bcc300ca152ec3f2b705bf7e6a7482eaed9a1db7
Maria <arkhipovam23@gmail.com>
2022-08-18 09:34:54 +0600

    first

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+  $('.design-slider').slick({
+    dots: false,
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow: '<img class="arrow arrow-left" src="/images/Arrow l.svg" alt="">',
+    nextArrow: '<img class="arrow arrow-right" src="/images/Arrow r.svg" alt="">',
+    
+  });
+
+});
